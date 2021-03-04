5607af122682408502f99c1c3cfda3f1757830ed
Lilia <lilia.harireche@gmail.com>
2021-03-04 20:20:03 +0100

    add evaluate et util_GENDER

---
+++ util_GENDER.py
@@ -0,0 +1,42 @@
+""" 
+module for create class GENDER with functions to do some manipulations of gender
+"""
+# External imports
+
+import spacy
+import pdb
+from pprint import pprint
+from pprint import pformat
+from docopt import docopt
+from collections import defaultdict
+from operator import itemgetter
+
+from enum import Enum
+from spacy.tokens.token import Token
+from typing import Dict
+
+# Local imports
+
+#=-----
+
+class GENDER(Enum):
+    """
+    Enumerate possible genders.
+    Ignore option resolves to words that should be ignored in particular language
+    """
+    male = 0
+    female = 1
+    neutral = 2
+    unknown = 3
+    ignore = 4 
+
+# Winobias gender type conversion
+WB_GENDER_TYPES = {
+    "male": GENDER.male,
+    "female": GENDER.female,
+    "neutral": GENDER.neutral,
+}
+
+
+
+
