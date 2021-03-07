e1dc8b6646a1f58e834309d227c038b08b44ecbb
AntoineRodriguez <antoine.rodriguez@devoteam.com>
2021-03-07 11:58:42 +0100

    add anti_pro_split.py and create pro-anti_indexes update .gitignore update evaluation.py add test.align

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -1,21 +1,14 @@
 """
-evaluate betwwen gold_gender(true_gender) and pred_gender(predicted_gender) using 
+evaluate betwen gold_gender(true_gender) and pred_gender(predicted_gender) using
 accuracy, precesion, recall, f1-score.
 """
-
-# External imports
-import pdb
-import json
 from pprint import pprint
-from pprint import pformat
-from docopt import docopt
 from collections import defaultdict
-from operator import itemgetter
+# from operator import itemgetter
 from typing import List, Dict
 
-# Local imports
-from util_GENDER import GENDER, WB_GENDER_TYPES
-#=-----
+from languages.util import GENDER, WB_GENDER_TYPES
+
 
 def calc_f1(precision: float, recall: float) -> float:
     """
@@ -25,42 +18,42 @@ def calc_f1(precision: float, recall: float) -> float:
 
 
 def evaluate_bias(ds: List[str], predicted: List[GENDER]) -> Dict:
-    
     """
     function to evaluate between gold_gender and pred_gender
     Get performance metrics for gender bias.
     
-    ds: list contains strings
-    predicted: list of gender    
+    ds: list contains strings of sentences
+    predicted: list of gender predicted
     """
-    assert(len(ds) == len(predicted))  # must have same length to create tuples
+    assert (len(ds) == len(predicted))  # must have same length to create tuples
+
     prof_dict = defaultdict(list)
     conf_dict = defaultdict(lambda: defaultdict(lambda: 0))
-    total = defaultdict(lambda: 0)    # increment values if we have any gender
-    pred_cnt = defaultdict(lambda: 0) 
-    correct_cnt = defaultdict(lambda: 0)  #increment values if true_gender == predicted_gender
+    total = defaultdict(lambda: 0)  # increment values if we have any gender
+    pred_cnt = defaultdict(lambda: 0)
+    correct_cnt = defaultdict(lambda: 0)  # increment values if true_gender == predicted_gender
 
     count_unknowns = defaultdict(lambda: 0)
 
     for (gold_gender, word_ind, sent, profession), pred_gender in zip(ds, predicted):
-                                                                  #tuples of values in ds and values in predicted 
+        # tuples of values in ds and values in predicted
         if pred_gender == GENDER.ignore:
-            continue # skip analysis of ignored words
+            continue  # skip analysis of ignored words
 
-        gold_gender = WB_GENDER_TYPES[gold_gender] #allows Winobias gender type conversion 
+        gold_gender = WB_GENDER_TYPES[gold_gender]  # allows Winobias gender type conversion
 
         if pred_gender == GENDER.unknown:
-            count_unknowns[gold_gender] += 1  #increment values for any unknown pred_gender 
+            count_unknowns[gold_gender] += 1  # increment values for any unknown pred_gender
 
         sent = sent.split()
-        
+
         profession = profession.lower()
-        if not profession:
-            pdb.set_trace()
+        # if not profession:
+        #    pdb.set_trace()
 
-        total[gold_gender] += 1   
+        total[gold_gender] += 1
 
-        if pred_gender == gold_gender:      
+        if pred_gender == gold_gender:
             correct_cnt[gold_gender] += 1
 
         pred_cnt[pred_gender] += 1
@@ -70,28 +63,37 @@ def evaluate_bias(ds: List[str], predicted: List[GENDER]) -> Dict:
 
     prof_dict = dict(prof_dict)
     all_total = sum(total.values())
-    
-    acc = round((sum(correct_cnt.values()) / all_total) * 100, 1)  #calcul accuracy
-    
-    recall_male = round((correct_cnt[GENDER.male] / total[GENDER.male]) * 100, 1)  #calcul metrics for male
-    prec_male = round((correct_cnt[GENDER.male] / pred_cnt[GENDER.male]) * 100, 1)
-    f1_male = round(calc_f1(prec_male, recall_male), 1)
-    
-    recall_female = round((correct_cnt[GENDER.female] / total[GENDER.female]) * 100, 1)  #calcul metrics for female
-    prec_female = round((correct_cnt[GENDER.female] / pred_cnt[GENDER.female]) * 100, 1)
-    f1_female = round(calc_f1(prec_female, recall_female), 1)
 
-	# ENREGISTRER DANS UN FICHIER TEXTE
-    output_dict = {"acc": acc,
-                   "f1_male": f1_male,
-                   "f1_female": f1_female,
-                   "unk_male": count_unknowns[GENDER.male],
-                   "unk_female": count_unknowns[GENDER.female],
-                   "unk_neutral": count_unknowns[GENDER.neutral]}
-    #print(json.dumps(output_dict))
-    
-    return output_dict  ## à vérifier
-		"""
+    output_dict = {}  # init output dictionnary
+
+    # Compute metrics
+    accuracy = round((sum(correct_cnt.values()) / all_total) * 100, 1)  # compute accuracy
+    output_dict['acc'] = accuracy
+
+    if (total[GENDER.male] != 0) & (pred_cnt[GENDER.male] != 0):  # Avoid ZeroDivisionError
+        recall_male = round((correct_cnt[GENDER.male] / total[GENDER.male]) * 100, 1)  # compute metrics for male
+        prec_male = round((correct_cnt[GENDER.male] / pred_cnt[GENDER.male]) * 100, 1)
+        f1_male = round(calc_f1(prec_male, recall_male), 1)
+        output_dict['f1_male'] = f1_male
+    else:
+        output_dict['f1_male'] = None
+
+    if (total[GENDER.female] != 0) & (pred_cnt[GENDER.female] != 0): # Avoid ZeroDivisionError
+        recall_female = round((correct_cnt[GENDER.female] / total[GENDER.female]) * 100, 1)  # calcul metrics for female
+        prec_female = round((correct_cnt[GENDER.female] / pred_cnt[GENDER.female]) * 100, 1)
+        f1_female = round(calc_f1(prec_female, recall_female), 1)
+        output_dict['f1_female'] = f1_female
+    else:
+        output_dict['f1_female'] = None
+
+    output_dict['unk_male'] = count_unknowns[GENDER.male]
+    output_dict['unk_female'] = count_unknowns[GENDER.female]
+    output_dict['unk_neutral'] = count_unknowns[GENDER.neutral]
+
+    return output_dict
+
+
+"""
     male_prof = [prof for prof, vals in prof_dict.items()  #collect professions of male, female, neutral, amb
                  if all(pred_gender == GENDER.male
                         for pred_gender
@@ -109,5 +111,13 @@ def evaluate_bias(ds: List[str], predicted: List[GENDER]) -> Dict:
 
     amb_prof = [prof for prof, vals in prof_dict.items()
                 if len(set(map(itemgetter(0), vals))) != 1]
-"""  ### UTILE ? sinon supprimer
+"""
 
+if __name__ == '__main__':
+
+    # Test evaluate_bias
+    ds = [['female', 1, 'The developer argued with the designer because she did not like the design.', 'developer'],
+          ['male', 5, 'The developer argued with the designer because his idea cannot be implemented.', 'designer']]
+    predicted = [GENDER.male, GENDER.male]  # accuracy 0.5
+
+    pprint(evaluate_bias(ds, predicted))
